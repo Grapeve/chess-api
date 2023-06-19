3ad6e8ad91928047ecfcdad0e7ada3b87e5bde90
Rhys <2068188418@qq.com>
2023-06-19 18:06:56 +0800

    feat: recordChessboardStatus

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 import json
 import time
-from utils import predictAction
+from utils import predictAction, recordChessboardStatus
 
 app = FastAPI()
 
@@ -19,14 +20,13 @@ def hello_world():
     )
 
 
-@app.get("/chess/{chessboard_status}")
-def read_item(chessboard_status: str, move_status: str):
+@app.get("/predictChessAction/{chessboard_status}")
+def read_item(chessboard_status: str):
     """
-    chessboard_status: 棋子移动前的棋局状态 \n
-    move_status: 棋子移动状态
+    chessboard_status: 棋子移动后的棋局状态 \n
     """
     start_time = time.time()
-    possible_chess_moving = predictAction(chessboard_status, move_status)
+    possible_chess_moving = predictAction(chessboard_status)
     possible_chess_moving = [json.loads(json_str) for json_str in possible_chess_moving]
     end_time = time.time()
     print("spend_time: ", end_time - start_time)
@@ -36,6 +36,32 @@ def read_item(chessboard_status: str, move_status: str):
             "possible_chess_moving": possible_chess_moving,
         }
     )
+
+
+class ChessboardData(BaseModel):
+    chessboardStatus: str
+    predictAction: str
+    chessboardStatusAfterPredictAction: str
+    result: str
+
+
+@app.post("/recordChessStatus")
+def record_chess_status(data: ChessboardData):
+    record_status = recordChessboardStatus(data)
+    if record_status:
+        return JSONResponse(
+            status_code=200,
+            content={
+                "message": "successful",
+            }
+        )
+    else:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "message": "successful",
+            }
+        )
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,21 +1,21 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, split, col, count, lit
 from pyspark.sql.window import Window
+import csv
 
 
-def predictAction(chessboardStatus, moveStatus):
+def predictAction(chessboardStatus):
     # 创建SparkSession
     spark = SparkSession.builder.appName("predictAction").getOrCreate()
     # 读取数据
-    df_chess = spark.read.csv("chess.csv", header=False)
-    df_chess = df_chess.withColumnRenamed("_c0", "chessboradStatus") \
-        .withColumnRenamed("_c1", "moveStatus") \
-        .withColumnRenamed("_c2", "chessboradStatusAfterMoving") \
-        .withColumnRenamed("_c3", "predictAction") \
-        .withColumnRenamed("_c4", "result")
+    df_chess = spark.read.csv("chessboard.csv", header=False)
+    df_chess = df_chess.withColumnRenamed("_c0", "chessboardStatus") \
+        .withColumnRenamed("_c1", "predictAction") \
+        .withColumnRenamed("_c2", "chessboardStatusAfterPredictAction") \
+        .withColumnRenamed("_c3", "result")
     # 根据输入的棋局状态和棋子移动状态去查找
     filtered_df_chess = df_chess.filter(
-        (col("chessboradStatus") == chessboardStatus) & (col("moveStatus") == moveStatus))
+        (col("chessboardStatus") == chessboardStatus))
     # 统计重复数据出现次数
     result_df_chess = filtered_df_chess.groupBy(filtered_df_chess.columns).agg(count("*").alias("count"))
     # 排序
@@ -25,8 +25,24 @@ def predictAction(chessboardStatus, moveStatus):
     return result_df_chess.toJSON().collect()
 
 
+def recordChessboardStatus(chessboardData):
+    try:
+        with open('recordChessboard.csv', 'a', newline='') as f:
+            csvwriter = csv.writer(f)
+            row_data = [
+                chessboardData.chessboardStatus,
+                chessboardData.predictAction,
+                chessboardData.chessboardStatusAfterPredictAction,
+                chessboardData.result
+            ]
+            csvwriter.writerow(row_data)
+            return True
+    except Exception as e:
+        print(f'An error occurred: {e}')
+        return False
+
+
 if __name__ == "__main__":
-    chessboard_status = "0919293949596979891777062646668600102030405060708012720323436383"
-    move_status = "7747"
-    possible_chess_moving = predictAction(chessboard_status, move_status)
+    chessboard_status = "0919293949596979891747062646668600102030405060708012720323436383"
+    possible_chess_moving = predictAction(chessboard_status)
     print(possible_chess_moving)
